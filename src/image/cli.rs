f102c2addaf8f9a86134bd63d848d1919a76fee9
dreadster3 <afonso.antunes@live.com.pt>
2023-05-05 02:03:40 +0000

    Rebase master

--- src/image/cli.rs
+++ src/image/cli.rs
@@ -1,27 +1,37 @@
-use crate::image::resize::{ResizeCommand, ResizeError};
 use clap::Subcommand;
+
+use crate::image::convert::{ConvertCommand, ConvertError};
+use crate::image::resize::{ResizeCommand, ResizeError};
 
 #[derive(Subcommand)]
 pub enum ImageCommand {
     #[clap(name = "resize")]
     Resize(ResizeCommand),
+
+    #[clap(name = "convert")]
+    Convert(ConvertCommand),
 }
 
 #[derive(Debug)]
 pub enum ImageError {
     ResizeError(ResizeError),
+    ConvertError(ConvertError),
     NoInputError,
 }
 
 impl ImageCommand {
     pub fn execute(&self, input: Option<&str>) -> Result<(), ImageError> {
-        match &self {
-            ImageCommand::Resize(resize) => match input {
-                Some(input) => resize
+        match input {
+            Some(input) => match self {
+                ImageCommand::Resize(resize) => resize
                     .execute(&input)
                     .map_err(|e| ImageError::ResizeError(e)),
-                None => Err(ImageError::NoInputError),
+
+                ImageCommand::Convert(convert) => convert
+                    .execute(&input)
+                    .map_err(|e| ImageError::ConvertError(e)),
             },
+            None => Err(ImageError::NoInputError),
         }
     }
 }
